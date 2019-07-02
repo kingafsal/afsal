75923b96dde640cfb3366b2edb6c0c5b76198f60
kingafsal <52131034+kingafsal@users.noreply.github.com>
2019-07-02 09:55:37 +0530

    Create c1.java

---
+++ c1.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG {
+    
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      int m=sc.nextInt();
+      System.out.print(Math.pow(n,m));
+    
+    }
+}
